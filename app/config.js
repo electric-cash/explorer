3a5eef26fba3d2161e2c68ff17314451f1070082
Ian Duoteli Fleming <54151411+ianduoteli@users.noreply.github.com>
2019-09-30 19:22:56 +0000

    Minor fix of default coin

--- app/config.js
+++ app/config.js
@@ -5,7 +5,7 @@ var url = require('url');
 var coins = require("./coins.js");
 var credentials = require("./credentials.js");
 
-var currentCoin = process.env.BTCEXP_COIN || "BTCR";
+var currentCoin = process.env.BTCEXP_COIN || "BTC"; // BTC is the same as BTCR
 
 var rpcCred = credentials.rpc;
 
