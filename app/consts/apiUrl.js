f23ae02c66d0413a8082d5b9c3b55b996c1dcaa2
Michał Czekała <michal.czekala@cloudbest.com>
2021-08-09 15:24:47 +0200

    fix(homepage): change price feed provider

--- app/consts/apiUrl.js
+++ app/consts/apiUrl.js
@@ -1,4 +1,4 @@
-const EXCHANGE_RATE_USDT = "https://openapi-exchange.coinbene.com/api/v3/spot/instruments/ticker_one?instrument_id=ELCASH/USDT";
+const EXCHANGE_RATE_USDT = "https://api.coingecko.com/api/v3/simple/price?ids=electric-cash&vs_currencies=usd";
 const EXCHANGE_RATE_BTC = "https://openapi-exchange.coinbene.com/api/v3/spot/instruments/ticker_one?instrument_id=ELCASH/BTC";
 const EXCHANGE_RATE_USDT_USD = "https://api.bitfinex.com/v1/pubticker/ustusd";
 
